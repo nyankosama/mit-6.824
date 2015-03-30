f36d491930f2fc683f258eceeba13d262eb428f6
nyankosama <825138000@qq.com>
2015-03-30 22:54:17 +0800

    update RpcTest for multi-services

--- src/test/java/com/nyankosama/test/RpcTest.java
+++ src/test/java/com/nyankosama/test/RpcTest.java
@@ -3,6 +3,7 @@ package com.nyankosama.test;
 import com.nyankosama.base.ResultSet;
 import com.nyankosama.rpc.RpcClient;
 import com.nyankosama.rpc.RpcServer;
+import org.junit.Test;
 
 /**
  * @created: 2015/3/29
@@ -23,7 +24,20 @@ public class RpcTest {
         }
     }
 
-    public static void main(String[] args) {
+    public static interface Echo {
+        public ResultSet<String, String> echo(String str);
+    }
+
+    public static class EchoImpl implements Echo {
+
+        @Override
+        public ResultSet<String, String> echo(String str) {
+            return new ResultSet<>(str);
+        }
+    }
+
+    @Test
+    public void benchmark() {
         RpcServer server = new RpcServer("127.0.0.1", 9123);
         server.regiester(new HelloImpl());
         new Thread(){
@@ -43,4 +57,23 @@ public class RpcTest {
         long end = System.currentTimeMillis();
         System.out.printf("cost: %dms, qps: %f \n", (end - begin), (double)total / (end - begin) * 1000);
     }
+
+    @Test
+    public void testMultiServices() {
+        RpcServer server = new RpcServer("localhost", 9123);
+        server.regiester(new HelloImpl());
+        server.regiester(new EchoImpl());
+        new Thread(){
+            @Override
+            public void run() {
+                server.start();
+            }
+        }.start();
+        RpcClient client = new RpcClient("localhost", 9123);
+        Hello hello = client.refer(Hello.class);
+        Echo echo = client.refer(Echo.class);
+        System.out.println(hello.sayHi("hlr").get());
+        System.out.println(echo.echo("echo").get());
+    }
+
 }
